40be00f555a35596b8e556bc6c390410a2c450bf
lpnl3456 <luke.peddle@keyin.com>
2024-06-17 14:17:34 -0230

    Add the index page and created the menu

---
+++ index.js
@@ -0,0 +1,34 @@
+global.DEBUG = true;
+
+const fs = require('fs');
+
+const myArgs = process.argv.slice(2);
+
+if(DEBUG) if(myArgs.length >= 1) console.log('myArgs: ', myArgs);
+
+switch(myArgs[0]) {
+    case 'init':
+    case 'i':
+      if (DEBUG) console.log(myArgs[0], '- init the app.');
+      initializeApplication();
+      break;
+    case 'config':
+    case 'c':
+      if (DEBUG) console.log(myArgs[0], '- create the config folders and files.');
+     //configApplication();
+      break;
+    case 'token':
+    case 't':
+      if (DEBUG) console.log(myArgs[0], '- manage the tokens');
+      //tokenApplication();
+      break;
+    case '--help':
+    case '--h':
+    default:
+      if (DEBUG) console.log(myArgs[0], '- display help');
+      //myEventEmitter.emit('event', process.argv, 'WARNING', 'Unrecognized CLI item requested.');
+      fs.readFile(__dirname + "/usage.txt", (error, data) => {
+        if(error) throw error;
+        console.log(data.toString());
+      });
+  }
