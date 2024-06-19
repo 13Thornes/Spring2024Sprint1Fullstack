66f1b70fe1b5bf318db4f2a615b93c6eebcf1fdd
lpnl3456 <luke.peddle@keyin.com>
2024-06-19 13:50:14 -0230

    Can reset the config file

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
+const { folders, configjson } = require('./templates');
 const myArgs = process.argv.slice(2);
+const myEventEmitter = require('./logEvents');
 
 function displayConfig() {
     if(DEBUG) console.log('config.displayConfig()');
@@ -40,32 +42,23 @@ function setConfig() {
 }
 
 function restartConfig() {
-    if(DEBUG) console.log('config.setConfig()');
-    if(DEBUG) console.log(myArgs);
-
-    let match = false;
-    fs.readFile(__dirname + "/json/config.json", (error, data) => {
-        if(error) throw error;         
-        if(DEBUG) console.log(JSON.parse(data));
-        let cfg = JSON.parse(data);
-        for(let key of Object.keys(cfg)){
-            if(DEBUG) console.log(`K E Y: ${key}`);
-            if(key === myArgs[2]) {
-                cfg[key] = myArgs[3];
-                match = true;
+    if(DEBUG) console.log('config.restartConfig()');
+    let configdata = JSON.stringify(configjson, null, 2);
+    let fileName = './json/config.json'
+    fs.writeFile(fileName, configdata, (err) => {
+        if(err) {
+            if (err.code == 'ENOENT') {
+                myEventEmitter.emit('event', fileName, 'ERROR', `The ${fileName} was in error, no file or directory.`);
+                console.log('No file or directory, has the directory been created.');
             }
+            else
+                console.log(err);
         }
-        if(!match) {
-            console.log(`invalid key: ${myArgs[2]}, try another.`)
-       }
-        if(DEBUG) console.log(cfg);
-        data = JSON.stringify(cfg, null, 2);
-        // looks like this code is writing the file again even if there is
-        fs.writeFile(__dirname + '/json/config.json', data, (error) => {
-            if (error) throw error;
-            if(DEBUG) console.log('Config file successfully updated.');
-        });
-    });
+        else {
+            myEventEmitter.emit('event', fileName, 'INFO', `The config file was successfully reseted to the original version`);
+            console.log('the Config file was rested to the original version.');
+        }
+    })
 }
 
 
@@ -79,6 +72,7 @@ function configApplication() {
       break;
   case '--reset':
       if(DEBUG) console.log('--reset');
+      restartConfig();
       break;
   case '--set':
       if(DEBUG) console.log('--set');
